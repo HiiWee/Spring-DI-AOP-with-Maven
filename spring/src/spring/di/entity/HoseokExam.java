93dc3af9f6a3bd6fbb862b7f83901110c325fd05
HiiWee <wpdlzhf159@gmail.com>
2022-01-03 03:57:05 +0900

    값 형식 DI

--- spring/src/spring/di/entity/HoseokExam.java
+++ spring/src/spring/di/entity/HoseokExam.java
@@ -17,4 +17,36 @@ public class HoseokExam implements Exam {
 		return total() / 4.0f;
 	}
 
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+
 }
