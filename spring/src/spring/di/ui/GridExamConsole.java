f8fff2fa0d77cbf534c496597a9bab5bc1e825f5
HiiWee <wpdlzhf159@gmail.com>
2022-01-04 23:52:03 +0900

    @Autowired의 위치와 required 옵션

--- spring/src/spring/di/ui/GridExamConsole.java
+++ spring/src/spring/di/ui/GridExamConsole.java
@@ -7,26 +7,40 @@ import spring.di.entity.Exam;
 
 // GridExamConsole = Exam출력시 격자 형태로 출력
 public class GridExamConsole implements ExamConsole {
+	
+	// 기본생성자를 호출하며 injection이 된다.
+	@Autowired(required = false)
+	@Qualifier("exam1")
 	private Exam exam;
 
 	public GridExamConsole() {
-		
+		System.out.println("constructor");
 	}
 	
-	public GridExamConsole(Exam exam) {
+	// 오버로드 생성자에서 Qualifier는 매개변수에 직접 설정해야함
+	//@Autowired
+	public GridExamConsole(/*@Qualifier("exam1")*/Exam exam) {
+		System.out.println("overloaded constructor");
 		this.exam = exam;
 	}
 
 	@Override
 	public void print() {
-		System.out.println("total :\t" + exam.total());
-		System.out.println("avg :\t" + exam.avg());
+		if (exam == null) {
+			System.out.println("total :\t" + 0);
+			System.out.println("avg :\t" + 0);
+		}
+		else {
+			System.out.println("total :\t" + exam.total());
+			System.out.println("avg :\t" + exam.avg());
+		}
 	}
 
-	@Autowired
-	@Qualifier("exam1")
+
+	// setter를 호출하며 injection 가능
 	@Override
 	public void setExam(Exam exam) {
+		System.out.println("setter");
 		this.exam = exam;
 	}
 
